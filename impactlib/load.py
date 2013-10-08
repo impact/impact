b4b89dd39e860f5882f492dd3ae92b197d369c1e
Michael M. Tiller <michael.tiller@gmail.com>
2013-10-08 15:00:45 -0400

    Got rid of debugging output

--- impactlib/load.py
+++ impactlib/load.py
@@ -3,7 +3,16 @@ import json
 
 from impactlib import config
 
+cached_data = None
+
 def load_repo_data():
+    global cached_data
+
+    # If we've already called this function once in this process, use
+    # the previously returned result.
+    if cached_data!=None:
+        return cached_data
+
     urls = config.get_repos()
     ret = {}
     # Ideally, we could cache this data if we cannot immediately
@@ -11,10 +20,10 @@ def load_repo_data():
     # allow you to search.  Install will require network connectivity
     # anyway.
     for url in urls:
-        print "Loading "+str(url)
         req = urllib2.Request(url)
         response = urllib2.urlopen(req)
         data = json.loads(response.read())
         ret.update(data)
 
+    cached_data = ret
     return ret
