5758134e6e550d33054a331fc113efb3748b8c58
Dietmar Winkler <dietmar.winkler@dwe.no>
2013-12-11 08:22:21 +0100

    Changing the version number because of change in the schema.
    See also #36.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="impactlib",
-      version="0.1.0",
+      version="0.2.0",
       description="Modelica package manager",
       author="Michael Tiller",
       author_email="michael.tiller@gmail.com",
