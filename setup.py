a97383a321bdd3a3ec3b25428f71356d522da84c
Dietmar Winkler <dietmar.winkler@dwe.no>
2014-01-17 10:50:11 +0100

    bump version number to 0.2.2. to reflect the fixed packaging.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name="impact",
-      version="0.2.1",
+      version="0.2.2",
       description="Modelica package manager",
       long_description=open(join(dirname(__file__), 'README.md')).read(),
       author="Michael Tiller",
