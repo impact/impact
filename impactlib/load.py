f85dd38fbde0ee4f109cb6ac93d0c74ad97a9474
Michael M. Tiller <michael.tiller@gmail.com>
2014-02-20 09:55:37 -0500

    This incorporates a fix for #55
    But I still haven't tested it on Windows.

--- impactlib/load.py
+++ impactlib/load.py
@@ -5,6 +5,31 @@ from impactlib import config
 
 cached_data = None
 
+### I stole this from http://bugs.python.org/issue11220 ###
+import httplib, ssl, socket
+
+class HTTPSConnectionV3(httplib.HTTPSConnection):
+    def __init__(self, *args, **kwargs):
+        httplib.HTTPSConnection.__init__(self, *args, **kwargs)
+        
+    def connect(self):
+        sock = socket.create_connection((self.host, self.port), self.timeout)
+        if self._tunnel_host:
+            self.sock = sock
+            self._tunnel()
+        try:
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
+                                        ssl_version=ssl.PROTOCOL_SSLv3)
+        except ssl.SSLError, e:
+            print("Trying SSLv23.")
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
+                                        ssl_version=ssl.PROTOCOL_SSLv23)
+            
+class HTTPSHandlerV3(urllib2.HTTPSHandler):
+    def https_open(self, req):
+        return self.do_open(HTTPSConnectionV3, req)
+######
+
 def load_repo_data():
     global cached_data
 
@@ -21,6 +46,7 @@ def load_repo_data():
     # anyway.
     for url in urls:
         try:
+            urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
             req = urllib2.Request(url)
             response = urllib2.urlopen(req)
             data = json.loads(response.read())
