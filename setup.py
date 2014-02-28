a634ac4217953209ddf3c916859565d62fcba9c0
Dietmar Winkler <dietmar.winkler@dwe.no>
2014-02-28 13:30:11 +0100

    Bump version number to 0.5.0
    Version 0.5.0 contains now:
     * #18 Add a "list" feature also with improved colouring #56
     * #39 sorted output for search
     * #17 configurable installation directory
     * #15 dryrun option does no longer download lib files
     * #52 store hashes for tags with
     * #46 executable name is now `impact` already since v0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name="impact",
-      version="0.3.0",
+      version="0.5.0",
       description="Modelica package manager",
       long_description=open(join(dirname(__file__), 'README.md')).read(),
       author="Michael Tiller",
