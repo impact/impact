22201865bd348102c6f0054885158d4ffb421c54
Michael Tiller <michael.tiller@gmail.com>
2014-07-13 18:18:32 -0400

    A bit more work on install, but still a few more things to do
    First, I need to determine the latest version of a given library.
    Then I need to go through and process all libraries and determine
    all dependencies and make sure there are no errors in them.
    Finally, run through the various libraries identified and install them.

--- cmds/install.go
+++ cmds/install.go
@@ -2,6 +2,7 @@ package cmds
 
 import "github.com/wsxiaoys/terminal/color"
 import "errors"
+import "strings"
 
 /* Define a struct listing all command line options for 'install' */
 type InstallCommand struct {
@@ -15,8 +16,22 @@ func (x *InstallCommand) Execute(args []string) error {
 		return errors.New("No libraries requested for installation");
 	}
 	for _, arg := range(args) {
+		var lib = arg
+		var ver = "";
+		parts := strings.Split(arg, "#");
+
+		if (len(parts)==1) {
+			// TODO: Determine latest version
+		} else if (len(parts)==2) {
+			lib = parts[0];
+			ver = parts[1];
+		} else if (len(parts)>2) {
+			return errors.New("Invalid version specification: "+arg+
+				" (must be libraryName#version)");
+		}
+
 		if (x.Verbose) {
-			color.Println("@!Installing @{c}"+string(arg));
+			color.Println("@!Installing @{c}"+string(lib)+", version "+string(ver));
 		}
 	}
 	return nil;
