c2bf95bb5ff18148771fe5817a151811684ebab4
Michael Tiller <michael.tiller@gmail.com>
2014-12-08 17:18:45 -0500

    Added search functionality

--- deps/graph.go
+++ deps/graph.go
@@ -1,70 +1,212 @@
 package deps
 
+import "fmt"
+import "log"
+import "github.com/blang/semver"
+
+/*
+ * Create a special type to specifically represent library names.  This just
+ * helps make the API clearer.
+ */
 type LibraryName string
 
+/*
+ * This struct is not exported.  It is used to represent a unique library
+ * (i.e., name + version)
+ */
 type uniqueLibrary struct {
 	name LibraryName
-	ver  SemanticVersion
+	ver  *semver.Version
 }
 
+/*
+ * This is an edge in our (directed) dependency graph.  It indicates that `library`
+ * depends on `dependsOn`.  Each is represented as a unique library (i.e., name + version)
+ */
 type dependency struct {
 	library   uniqueLibrary
 	dependsOn uniqueLibrary
 }
 
+/*
+ * This type represents a specific configuration of libraries.  This is used to represent
+ * the resolution of dependencies.
+ */
+type Configuration map[LibraryName]*semver.Version
+
+func (conf Configuration) Clone() Configuration {
+	clone := Configuration{}
+	for k, v := range conf {
+		clone[k] = v
+	}
+	return clone
+}
+
+/*
+ * This type represents remaining possible values for a given library
+ */
+type Available map[LibraryName]*VersionList
+
+func (a Available) Clone() Available {
+	clone := Available{}
+	for k, v := range a {
+		clone[k] = v
+	}
+	return clone
+}
+
+func (a Available) Refine(subset Available) Available {
+	ret := Available{}
+
+	for k, v := range a {
+		v2, exists := subset[k]
+		if !exists {
+			ret[k] = v
+		} else {
+			ret[k] = (*v).Intersection(*v2)
+		}
+	}
+	return ret
+}
+
+func (a Available) Empty() []LibraryName {
+	ret := []LibraryName{}
+	for k, v := range a {
+		if v.Len() == 0 {
+			ret = append(ret, k)
+		}
+	}
+	return ret
+}
+
+/*
+ * A library index is simply a list of dependencies (edges)
+ */
 type LibraryIndex struct {
 	libraries []dependency
 }
 
+/*
+ * This function creates a new LibraryIndex object.
+ */
 func MakeLibraryIndex() LibraryIndex {
 	return LibraryIndex{
 		libraries: []dependency{},
 	}
 }
 
-func (index *LibraryIndex) AddDependency(lib LibraryName, libver SemanticVersion,
-	dep LibraryName, depver SemanticVersion) {
+/*
+ * Method to add a new dependency to a library index
+ */
+func (index *LibraryIndex) AddDependency(lib LibraryName, libver *semver.Version,
+	deplib LibraryName, depver *semver.Version) {
 
 	library := uniqueLibrary{name: lib, ver: libver}
-	dependsOn := uniqueLibrary{name: dep, ver: depver}
-	index.libraries = append(index.libraries, dependency{library: library, dependsOn: dependsOn})
+	dependsOn := uniqueLibrary{name: deplib, ver: depver}
+	dep := dependency{library: library, dependsOn: dependsOn}
+
+	index.libraries = append(index.libraries, dep)
 }
 
-func addVersion(vers []SemanticVersion, v SemanticVersion) []SemanticVersion {
-	l := len(vers)
-	if l == 0 {
-		return []SemanticVersion{v}
-	}
+/*
+ * Builds a list of all versions of a given library known to the
+ * index.  These are returned in sorted order (latest to earliest)
+ */
+func (index LibraryIndex) Versions(lib LibraryName) *VersionList {
+	present := map[*semver.Version]bool{}
 
-	compFirst := v.Compare(vers[0])
-	if compFirst == ComesBefore {
-		return append([]SemanticVersion{v}, vers...)
-	}
-
-	compLast := v.Compare(vers[l-1])
-	if compLast == ComesAfter {
-		return append(vers, v)
-	}
-
-	ret := []SemanticVersion{}
-	for i := 0; i < l-1; i++ {
-		v1 := vers[i]
-		ret = append(ret, v1)
-		v2 := vers[i+1]
-		if v1.Compare(v) == ComesBefore && v.Compare(v2) == ComesBefore {
-			ret = append(ret, v)
+	for _, dep := range index.libraries {
+		if dep.library.name == lib {
+			present[dep.library.ver] = true
 		}
 	}
-	ret = append(ret, vers[l-1])
-	return ret
+
+	log.Printf("Present: %#v", present)
+
+	vl := NewVersionList()
+	for v, _ := range present {
+		vl.Add(v)
+		log.Printf("vl = %s", vl.String())
+	}
+
+	vl.ReverseSort()
+	return vl
 }
 
-func (index LibraryIndex) Versions(lib LibraryName) []SemanticVersion {
-	ret := []SemanticVersion{}
-	for _, v := range index.libraries {
-		if v.library.name == lib {
-			ret = addVersion(ret, v.library.ver)
-		}
+func (index LibraryIndex) findFirstDepthFirst(mapped Configuration, avail Available, rest ...LibraryName) (config Configuration, err error) {
+	// Nothing left to process...we are done!
+	if len(rest) == 0 {
+		return mapped, nil
 	}
-	return ret
+
+	// Consider the next library in the list
+	lib := rest[0]
+	rest = rest[1:]
+
+	// Determine all versions known for current library
+	vers, constrained := avail[lib]
+	if !constrained {
+		vers = index.Versions(LibraryName(lib))
+	}
+
+	// Loop over each of those versions
+	for _, ver := range *vers {
+		log.Printf("Considering version %v of %s", ver, lib)
+
+		/* Create our own local copy of the configuration so we don't mutate mapped */
+		config = mapped.Clone()
+		// Create a list of versions that the current library depends on
+		depvers := Available{}
+		// Any new libraries
+		newlibs := []LibraryName{}
+
+		// Loop over depdencies
+		for _, dep := range index.libraries {
+			// Is this a dependency for the current library and version?
+			if dep.library.name == lib && ver.Compare(dep.library.ver) == 0 {
+				// If so, add it to the available set (if one exists)
+				dver, found := depvers[dep.dependsOn.name]
+				if !found {
+					dver = NewVersionList()
+					depvers[dep.dependsOn.name] = dver
+				}
+				dver.Add(dep.dependsOn.ver)
+			}
+		}
+
+		// Add any new dependencies
+		for n1, _ := range depvers {
+			found := false
+			for _, n2 := range rest {
+				if n1 == n2 {
+					found = true
+				}
+			}
+			if !found {
+				newlibs = append(newlibs, n1)
+			}
+		}
+
+		// Take the intersection of the available version with the dependent versions
+		intersection := avail.Refine(depvers)
+
+		// Are any of the available value sets empty?  If so, return an error
+		empty := intersection.Empty()
+		if len(empty) > 0 {
+			err = fmt.Errorf("No compatible versions of: %v", empty)
+			return
+		}
+
+		// Find the current library and version choice
+		config[lib] = ver
+
+		// Recurse to solve remaining variables
+		newlibs = append(newlibs, rest...)
+		return index.findFirstDepthFirst(config, intersection, newlibs...)
+	}
+	return
+}
+
+func (index LibraryIndex) Resolve(libraries ...LibraryName) (config Configuration, err error) {
+	return index.findFirstDepthFirst(config, Available{}, libraries...)
 }
--- deps/graph_test.go
+++ deps/graph_test.go
@@ -1,14 +1,29 @@
 package deps
 
+import "log"
 import "testing"
+import "github.com/blang/semver"
 import "github.com/stretchr/testify/assert"
 
 func TestResolution(t *testing.T) {
 	index := MakeLibraryIndex()
-	root1 := MakeSemVer(1, 0, 0)
-	a1 := MakeSemVer(1, 0, 0)
+	root1, err := semver.New("1.0.0")
+	assert.NoError(t, err, "Parsing root1 version")
+	a1, err := semver.New("1.0.0")
+	assert.NoError(t, err, "Parsing a1 version")
 	index.AddDependency("Root", root1, "A", a1)
 
 	rootVers := index.Versions("Root")
-	assert.Equal(t, rootVers, []SemanticVersion{root1})
+	assert.Equal(t, *rootVers, VersionList{root1})
+
+	// Introduce a circular dependency.  Make sure nothing breaks
+	index.AddDependency("A", a1, "Root", root1)
+
+	rootVers = index.Versions("Root")
+	assert.Equal(t, *rootVers, VersionList{root1})
+
+	config, err := index.Resolve("Root")
+	assert.NoError(t, err)
+
+	log.Printf("Configuration: %v", config)
 }
