3027c272e5633a1cb036f9c003e2ff4bf1cf5216
Michael Tiller <michael.tiller@gmail.com>
2015-04-03 14:46:53 -0400

    Converted another test to goconvey...missed this the first time

--- graph/versions_test.go
+++ graph/versions_test.go
@@ -1,63 +1,72 @@
 package graph
 
-import "testing"
-import "github.com/blang/semver"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"github.com/blang/semver"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "github.com/xogeny/xconvey"
+)
 
 func TestVersionConstruction(t *testing.T) {
-	vl := NewVersionList()
-	assert.Equal(t, 0, vl.Len())
+	Convey("Testing version construction", t, func(c C) {
+		vl := NewVersionList()
+		Equals(c, 0, vl.Len())
 
-	v1, err := semver.New("1.0.0")
-	assert.NoError(t, err)
+		v1, err := semver.New("1.0.0")
+		NoError(c, err)
 
-	vl.Add(v1)
-	assert.Equal(t, 1, vl.Len())
+		vl.Add(v1)
+		Equals(c, 1, vl.Len())
+	})
 }
 
 func TestOrdering(t *testing.T) {
-	vers := NewVersionList()
+	Convey("Testing version construction", t, func(c C) {
+		vers := NewVersionList()
 
-	names := []string{
-		"1.1.0",
-		"0.0.2",
-		"1.0.0",
-		"1.0.2-beta.1",
-		"1.0.2",
-		"0.2.1+a8cc9de3f",
-		"0.1.1-alpha1+HOTFIX1",
-		"0.0.1",
-		"2.1.0",
-		"1.0.2-alpha.1",
-		"1.0.1",
-		"0.1.0",
-	}
+		names := []string{
+			"1.1.0",
+			"0.0.2",
+			"1.0.0",
+			"1.0.2-beta.1",
+			"1.0.2",
+			"0.2.1+a8cc9de3f",
+			"0.1.1-alpha1+HOTFIX1",
+			"0.0.1",
+			"2.1.0",
+			"1.0.2-alpha.1",
+			"1.0.1",
+			"0.1.0",
+		}
 
-	for _, n := range names {
-		v, err := semver.New(n)
-		assert.NoError(t, err)
-		vers.Add(v)
-	}
+		for _, n := range names {
+			v, err := semver.New(n)
+			NoError(c, err)
+			vers.Add(v)
+		}
 
-	vers.Sort()
+		vers.Sort()
 
-	for i := 0; i < vers.Len()-1; i++ {
-		v1 := vers.Get(i)
-		v2 := vers.Get(i + 1)
-		assert.True(t, v1.LT(v2))
-		assert.True(t, v2.GT(v1))
-		assert.Equal(t, 0, v1.Compare(v1))
-		assert.Equal(t, 0, v2.Compare(v2))
-	}
+		for i := 0; i < vers.Len()-1; i++ {
+			v1 := vers.Get(i)
+			v2 := vers.Get(i + 1)
+			IsTrue(c, v1.LT(v2))
+			IsTrue(c, v2.GT(v1))
+			Equals(c, 0, v1.Compare(v1))
+			Equals(c, 0, v2.Compare(v2))
+		}
 
-	vers.ReverseSort()
+		vers.ReverseSort()
 
-	for i := 0; i < vers.Len()-1; i++ {
-		v2 := vers.Get(i)
-		v1 := vers.Get(i + 1)
-		assert.True(t, v1.LT(v2))
-		assert.True(t, v2.GT(v1))
-		assert.Equal(t, 0, v1.Compare(v1))
-		assert.Equal(t, 0, v2.Compare(v2))
-	}
+		for i := 0; i < vers.Len()-1; i++ {
+			v2 := vers.Get(i)
+			v1 := vers.Get(i + 1)
+			IsTrue(c, v1.LT(v2))
+			IsTrue(c, v2.GT(v1))
+			Equals(c, 0, v1.Compare(v1))
+			Equals(c, 0, v2.Compare(v2))
+		}
+	})
 }
