c50926ffa22fe46c5d08ebd1144d107cdd96aec9
Michael Tiller <michael.tiller@gmail.com>
2015-04-20 17:59:40 -0400

    Support for multiple libraries with the same name
    This closes #80

--- impact/search.go
+++ impact/search.go
@@ -16,6 +16,7 @@ type SearchCommand struct {
 	} `positional-args:"true" required:"true"`
 	All     bool `short:"a" long:"all" description:"Include all libraries in index"`
 	URL     bool `short:"u" long:"url" description:"Include homepage"`
+	Ratings bool `short:"r" long:"ratings" description:"Show ratings"`
 	Verbose bool `short:"v" long:"verbose" description:"Turn on verbose output"`
 }
 
@@ -27,6 +28,7 @@ func (x *SearchCommand) Execute(args []string) error {
 	}
 	term := x.Positional.Term
 	url := x.URL
+	ratings := x.Ratings
 
 	// Load user settings
 	settings, err := config.ReadSettings()
@@ -40,26 +42,50 @@ func (x *SearchCommand) Execute(args []string) error {
 		return fmt.Errorf("Error loading indices: %v", err)
 	}
 
-	g := ind.Group(settings.Choices)
-
-	if x.All {
-		// If we are going to consider all of them, sort them by rating
-		g = g.SortByRating()
-	}
+	g := ind.Group(settings.Choices).SortByRating()
 
 	for _, libs := range g.Libraries {
+		multiple := len(libs) > 0 && x.All
+		if multiple {
+			url = true     // Always show URLs with multiple matches
+			ratings = true // Always show ratings with multiple matches
+		}
 		for index, lib := range libs {
+			prefix := ""
 			if index > 0 && !x.All {
 				continue
+			}
+			if index > 0 {
+				prefix = "  "
 			}
 			// TODO: Should have some way of indicating the "chosen" one (i.e., what
 			// would be installed).
 			if lib.Matches(term) {
+				rstr := ""
+				if ratings {
+					if lib.Stars > 0 {
+						rstr = fmt.Sprintf(" [%d stars]", lib.Stars)
+					} else {
+						rstr = fmt.Sprintf(" [no rating]", lib.Stars)
+					}
+				}
+				if !x.All && len(libs) > 0 {
+					rstr = fmt.Sprintf("%s (use -a flag to see %d other libraries named %s)",
+						rstr, len(libs)-1, lib.Name)
+				}
+
+				if multiple && index == 0 {
+					color.Printf("@{y}First match in your search path:\n")
+				}
+				if multiple && index == 1 {
+					color.Printf("\n@{y}%sAlternatives:\n", prefix)
+				}
 				if url {
-					color.Printf("@{!}%s @{y}(%s):\n@{c} - %s\n", lib.Name, lib.Homepage,
-						lib.Description)
+					color.Printf("%s@{!}%s%s @{y}(%s):\n%s@{c} - %s\n", prefix, lib.Name,
+						rstr, lib.URI, prefix, lib.Description)
 				} else {
-					color.Printf("@{!}%s:\n@{c} - %s\n", lib.Name, lib.Description)
+					color.Printf("%s@{!}%s%s:\n%s@{c} - %s\n", prefix, lib.Name, rstr,
+						prefix, lib.Description)
 				}
 			}
 		}
--- index/group.go
+++ index/group.go
@@ -1,12 +1,38 @@
 package index
 
+import (
+	"sort"
+)
+
+type LibList []*Library
+
+func (l LibList) Len() int {
+	return len(l)
+}
+
+func (l LibList) Swap(i int, j int) {
+	x := l[i]
+	l[i] = l[j]
+	l[j] = x
+}
+
+func (l LibList) Less(i int, j int) bool {
+	if i == 0 {
+		return true
+	}
+	if j == 0 {
+		return false
+	}
+	return l[i].Stars > l[j].Stars
+}
+
 type GroupedIndex struct {
-	Libraries map[string][]*Library
+	Libraries map[string]LibList
 }
 
 func makeGroupedIndex() GroupedIndex {
 	return GroupedIndex{
-		Libraries: map[string][]*Library{},
+		Libraries: map[string]LibList{},
 	}
 }
 
@@ -20,20 +46,30 @@ func (i Index) Group(disamb map[string]string) GroupedIndex {
 			if selection && curi == lib.URI {
 				// If this is the selected version, put it at the
 				// front of the list
-				ret.Libraries[lib.Name] = append([]*Library{lib}, list...)
+				ret.Libraries[lib.Name] = append(LibList{lib}, list...)
 			} else {
 				// Otherwise, just add it to the list
 				ret.Libraries[lib.Name] = append(list, lib)
 			}
 		} else {
-			ret.Libraries[lib.Name] = []*Library{lib}
+			ret.Libraries[lib.Name] = LibList{lib}
 		}
 	}
 	return ret
 }
 
+// This sorts **all but the first** match by rating.  This is because the
+// first match is the one that is first in the install path so it should
+// always be listed first.  Among those not in the search path, this
+// rearranges them by rating.
 func (g GroupedIndex) SortByRating() GroupedIndex {
-	return g
+	ret := makeGroupedIndex()
+
+	for libname, libs := range g.Libraries {
+		sort.Sort(libs)
+		ret.Libraries[libname] = libs
+	}
+	return ret
 }
 
 // Return and index that has no "extra" libraries in it, only the ones that
